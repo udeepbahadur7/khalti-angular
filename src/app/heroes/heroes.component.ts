3738a6537d1d592d06132532b121b4d71a1a4ee7
Udeep Shrestha <udeepbahadur7@gmail.com>
2020-09-08 13:44:24 +0545

    Khalti checkout

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Hero } from '../core';
 import { HeroService } from './hero.service';
+import KhaltiCheckout from 'khalti-checkout-web';
 
 @Component({
   selector: 'app-heroes',
-  templateUrl: './heroes.component.html'
+  templateUrl: './heroes.component.html',
 })
 export class HeroesComponent implements OnInit {
   selected: Hero;
@@ -13,15 +14,50 @@ export class HeroesComponent implements OnInit {
   message = '?';
   heroToDelete: Hero;
   showModal = false;
+  checkout: any;
 
   constructor(private heroService: HeroService) {
     this.heroes$ = heroService.entities$;
+    let config = {
+      // replace this key with yours
+      publicKey: 'test_public_key_dc74e0fd57cb46cd93832aee0a390234',
+      productIdentity: '1234567890',
+      productName: 'Drogon',
+      productUrl: 'http://gameofthrones.com/buy/Dragons',
+      eventHandler: {
+        onSuccess(payload) {
+          // hit merchant api for initiating verfication
+          console.log(payload);
+        },
+        // onError handler is optional
+        onError(error) {
+          // handle errors
+          console.log(error);
+        },
+        onClose() {
+          console.log('widget is closing');
+        },
+      },
+      paymentPreference: [
+        'KHALTI',
+        'EBANKING',
+        'MOBILE_BANKING',
+        'CONNECT_IPS',
+        'SCT',
+      ],
+    };
+
+    let checkout = new KhaltiCheckout(config);
+    this.checkout = checkout;
   }
 
   ngOnInit() {
     this.getHeroes();
   }
 
+  handleKhaltiCheckout() {
+    this.checkout.show({ amount: 1000 });
+  }
   add(hero: Hero) {
     this.heroService.add(hero);
   }
